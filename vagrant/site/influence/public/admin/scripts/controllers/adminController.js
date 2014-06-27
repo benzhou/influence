358cbd4a570ae253b7053cc641648c93d067570b
Ben Zhou <bzhou@tritonloyalty.com>
2014-06-27 16:55:32 -0400

    recover file

---
+++ vagrant/site/influence/public/admin/scripts/controllers/adminController.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    var influenceAdminControllers = angular.module('influenceAdminControllers', []);
+
+    influenceAdminControllers.controller('influenceAdminCtrl', function ($scope) {
+        $scope.login = function(){
+
+        };
+    });
+}());
+
